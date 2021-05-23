57f66a851134b4d8066e0dd30592ee2f8467345b
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-23 10:25:12 +0530

    connect namelist to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import namelist from './components/namelist/namelist'
 
 function App() {
   return (
     <div>
-      <h1>name list</h1>
-      <hr />
-      <ul>
-        <li>Anuki gayara - software engineering</li>
-        <li>Sanduni Lanka - software engineering</li>
-        <li>chamodhi dewwandi - computer science</li>
-        <li>shamali pathirana - computer science</li>
-      </ul>
-      
-    </div>
+     <namelist />
+     </div>
   );
 }
 

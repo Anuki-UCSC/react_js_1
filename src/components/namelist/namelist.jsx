3067c4fa69bccf2e3da869f0190df666fabc1217
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-23 10:19:42 +0530

    namelist

---
+++ src/components/namelist/namelist.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function namelist(){
+    return(
+        <React.Fragment>
+        <h1>name list</h1>
+      <hr />
+      <ul>
+        <li>Anuki gayara - software engineering</li>
+        <li>Sanduni Lanka - software engineering</li>
+        <li>chamodhi dewwandi - computer science</li>
+        <li>shamali pathirana - computer science</li>
+      </ul>
+      </React.Fragment>
+    );
+}
+
+export default namelist;
+
+

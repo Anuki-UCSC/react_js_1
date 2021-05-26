2ea11b6746edaa01fda067166168f89c6d32ef2a
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-26 23:53:40 +0530

    new geader bar added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import NameList1 from './components/namelist/NameList1';
 import NameList2 from './components/namelist/NameList2';
 import NameList2_2 from './components/namelist/NameList2_2';
+import HeaderBar from './components/HeaderBar/HeaderBar';
 
 function App() {
   return (
     <div>
-     <h1>name list</h1>
+      <HeaderBar/>
+      <h1>name list</h1>
       <hr />
+      
       <NameList1/>
       <NameList2/>
       <hr />
---
+++ src/components/HeaderBar/HeaderBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeaderBar() {
+    return (
+        <div class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+      </div>
+        );
+}
+
+export default HeaderBar;
+

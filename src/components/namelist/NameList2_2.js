f3c2abe4e740af9218975bbc6d73faec28918a99
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-27 00:19:53 +0530

    bug corrected

--- src/components/namelist/NameList2_2.js
+++ src/components/namelist/NameList2_2.js
@@ -3,53 +3,53 @@ import React, {useState} from 'react';
 import NameListItem from './NameListItem';
 import NamelistDynamic2 from './NamelistDynamic2';
 
-const[namelist, setNameList]=useState([{
-    "name": 
-        {"title": "mr","first": "sherlock","last": "holmes"},
-    "location": 
-        {"city": "trinko","postcode": "93027",},
-    "email"
-        : "brad.gibson@example.com",
-    "dob": 
-        {"date": "1993-07-20T09:44:18.674Z","age": 26},
-    "phone":
-        "011-962-7516",  
-    "picture": 
-        {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg",  },
-},
-{
-    "name": 
-        {"title": "mr","first": "crab","last": "willston"},
-    "location": 
-        {"city": "colombo","postcode": "93027",},
-    "email"
-        : "brad.crab@example.com",
-    "dob": 
-        {"date": "1993-07-20T09:44:18.674Z","age": 26},
-    "phone":
-        "011-962-7516",  
-    "picture": 
-        {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg",  },
-},
-{
-    "name": 
-        {"title": "mr","first": "Hewan","last": "Seo ho"},
-    "location": 
-        {"city": "mathara","postcode": "93027",},
-    "email"
-        : "brad.gibson@example.com",
-    "dob": 
-        {"date": "1944-07-20T09:44:18.674Z","age": 26},
-    "phone":
-        "011-333-3333",  
-    "picture": 
-        {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
-}
-]);
+
 
 function NameList2_2()
 {
-    
+    const[namelist, setNameList]=useState([{
+        "name": 
+            {"title": "mr","first": "sherlock","last": "holmes"},
+        "location": 
+            {"city": "trinko","postcode": "93027",},
+        "email"
+            : "brad.gibson@example.com",
+        "dob": 
+            {"date": "1993-07-20T09:44:18.674Z","age": 26},
+        "phone":
+            "011-962-7516",  
+        "picture": 
+            {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg",  },
+    },
+    {
+        "name": 
+            {"title": "mr","first": "crab","last": "willston"},
+        "location": 
+            {"city": "colombo","postcode": "93027",},
+        "email"
+            : "brad.crab@example.com",
+        "dob": 
+            {"date": "1993-07-20T09:44:18.674Z","age": 26},
+        "phone":
+            "011-962-7516",  
+        "picture": 
+            {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg",  },
+    },
+    {
+        "name": 
+            {"title": "mr","first": "Hewan","last": "Seo ho"},
+        "location": 
+            {"city": "mathara","postcode": "93027",},
+        "email"
+            : "brad.gibson@example.com",
+        "dob": 
+            {"date": "1944-07-20T09:44:18.674Z","age": 26},
+        "phone":
+            "011-333-3333",  
+        "picture": 
+            {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
+    }
+    ]);    
 
 const namelistComponent=()=>{
     return namelist.map((aName)=>{
